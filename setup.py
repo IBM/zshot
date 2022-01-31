647df90ec956a1bd7643e0f1ec4b0f579a714bc6
Gabriele Picco <gabriele.picco@ibm.com>
2022-01-31 17:00:11 +0100

    Project template

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ version = '0.1'
 
 setup(name='zshot',
       version=version,
-      description="Zero and Few shot named entity recognition plugin for Spacy",
+      description="Zero and Few shot named entity recognition",
       long_description="""\
-Zero and Few shot named entity recognition plugin for Spacy""",
+Zero and Few shot named entity recognition""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='NER Zero-Shot Few-Shot',
       author='IBM Research',
