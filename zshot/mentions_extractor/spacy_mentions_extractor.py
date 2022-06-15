9578789b9a1b896964d88bc92a402ef18f82ac41
Gabriele Picco <Gabriele.Picco@ibm.com>
2022-06-15 11:37:25 +0100

    :zap: Exclude numeric, number, time from Spacy mentions extractor

--- zshot/mentions_extractor/spacy_mentions_extractor.py
+++ zshot/mentions_extractor/spacy_mentions_extractor.py
@@ -5,12 +5,15 @@ from zshot.mentions_extractor.mentions_extractor import MentionsExtractor
 
 
 class SpacyMentionsExtractor(MentionsExtractor):
+    
+    EXCLUDE_NER = ("CARDINAL", "DATE", "ORDINAL", "PERCENT", "QUANTITY", "TIME")
+
     def __init__(self):
         pass
 
     def extract_mentions(self, docs: List[Doc], batch_size: Optional[int] = None):
         for doc in docs:
-            ents_filtered = [ent for ent in doc.ents if ent.label_ not in "CARDINAL"]
+            ents_filtered = [ent for ent in doc.ents if ent.label_ not in self.EXCLUDE_NER]
             for mention in ents_filtered:
                 doc._.mentions.append(doc.char_span(mention.start_char, mention.end_char))
             doc.ents = []
