27b50cec480fc4f350066491e6cb65fb64f16b08
Gabriele Picco <gabriele.picco@ibm.comm>
2022-09-29 16:20:07 +0100

    :adhesive_bandage: Remove cache after tests passed
    Signed-off-by: Gabriele Picco <gabriele.picco@ibm.comm>
    Signed-off-by: Marcos Martinez <Marcos.Martinez.Galindo@ibm.com>
    Signed-off-by: Gabriele Picco <gabriele.picco@ibm.comm>

--- zshot/config.py
+++ zshot/config.py
@@ -1,7 +1,5 @@
 import os
 import pathlib
 
-from appdata import AppDataPaths
-
 MODELS_CACHE_PATH = os.getenv("MODELS_CACHE_PATH") if "MODELS_CACHE_PATH" in os.environ \
-    else AppDataPaths(f"{pathlib.Path(__file__).stem}").app_data_path + "/"
+    else f"{pathlib.Path.home()}/.cache/zshot/"
