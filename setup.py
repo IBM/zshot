6e8a86c68cf999c1e4888be3a1fcdcee80509790
Marcos Martinez <Marcos.Martinez.Galindo@ibm.com>
2022-10-17 12:22:28 +0100

    :art: Improved structure of setup and init.
    Signed-off-by: Marcos Martinez <Marcos.Martinez.Galindo@ibm.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
+from zshot import name, version, author, url, license_
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-version = '0.0.2'
 
-setup(name='zshot',
+setup(name=name,
       version=version,
       description="Zero and Few shot named entity recognition",
       long_description_content_type='text/markdown',
       long_description=long_description,
       classifiers=[],
       keywords='NER Zero-Shot Few-Shot',
-      author='IBM Research',
+      author=author,
       author_email='',
-      url='https://ibm.github.io/zshot',
-      license='MIT',
+      url=url,
+      license=license_,
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
--- zshot/__init__.py
+++ zshot/__init__.py
@@ -1,2 +1,8 @@
 from zshot.zshot import MentionsExtractor, Linker, Zshot, PipelineConfig  # noqa: F401
 from zshot.utils.displacy import displacy  # noqa: F401
+
+name = 'zshot'
+version = '0.0.2'
+url = 'https://ibm.github.io/zshot'
+license_ = 'MIT'
+author = 'IBM Research'
