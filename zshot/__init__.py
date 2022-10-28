86230b141cd0277434fe5892b5f6d7c084dc43ff
Gabriele Picco <piccogabriele@gmail.com>
2022-10-28 19:16:31 +0100

    :art: Code rafactor
    Signed-off-by: Gabriele Picco <piccogabriele@gmail.com>

--- zshot/__init__.py
+++ zshot/__init__.py
@@ -1,4 +1,4 @@
-from zshot.zshot import MentionsExtractor, Linker, Zshot, PipelineConfig  # noqa: F401
+from zshot.zshot import MentionsExtractor, Linker, Zshot, PipelineConfig, RelationsExtractor  # noqa: F401
 from zshot.utils.displacy import displacy  # noqa: F401
 
 __version__ = '0.0.4'
