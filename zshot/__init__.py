680f94664ce03fbd9b6a7697d55298dab7d590c8
Gabriele Picco <piccogabriele@gmail.com>
2022-10-28 17:11:04 +0100

    Add RE visualization

--- zshot/__init__.py
+++ zshot/__init__.py
@@ -1,4 +1,4 @@
 from zshot.zshot import MentionsExtractor, Linker, RelationsExtractor, Zshot, PipelineConfig  # noqa: F401
-from zshot.utils.displacy import displacy  # noqa: F401
+from zshot.utils.displacy.displacy import displacy  # noqa: F401
 
 __version__ = '0.0.3'
