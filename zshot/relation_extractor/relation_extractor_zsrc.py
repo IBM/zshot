5d4eba880c89e4c1780533edc12d93c44033abc2
Alberto Purpura <alp@ibm.com>
2022-11-08 10:24:54 +0000

    perf improv

--- zshot/relation_extractor/relation_extractor_zsrc.py
+++ zshot/relation_extractor/relation_extractor_zsrc.py
@@ -23,35 +23,33 @@ class RelationsExtractorZSRC(RelationsExtractor):
 
     def predict(self, docs: Iterator[Doc], batch_size=None) -> List[List[RelationSpan]]:
         relations_pred = []
-        if self.relations is not None:
-            relations_list = [rel.description for rel in self.relations]
-        else:
-            return relations_pred
-
-        for doc in tqdm(docs, desc='predicting relations'):
+        for doc in tqdm(docs, desc='classifying documents'):
             relations_doc = []
             items_to_process = []
             for i, e1 in enumerate(doc._.spans):
                 for j, e2 in enumerate(doc._.spans):
                     if (
-                        i == j or (e1, e2) in items_to_process or (
-                            e2, e1) in items_to_process
+                        i == j or (e1, e2) in items_to_process or (e2, e1) in items_to_process
                     ):
                         continue
                     else:
                         items_to_process.append((e1, e2))
-                    _, relations_probs = predict(
-                        self.model,
-                        [(e1, e2, doc.text)] * len(relations_list),
-                        relations_list,
-                        batch_size,
-                    )
-                    pred_class_idx = np.argmax(np.array(relations_probs))
-                    p = relations_probs[pred_class_idx]
-                    if p >= self.thr:
-                        relations_doc.append(
-                            RelationSpan(start=e1, end=e2, score=p,
-                                         relation=self.relations[pred_class_idx])
-                        )
+
+                    relations_probs = []
+                    if self.relations is not None:
+                        for rel in self.relations:
+                            _, probs = predict(
+                                self.model,
+                                [(e1, e2, doc.text)],
+                                rel.description,
+                                batch_size,
+                            )
+                            relations_probs.append(probs[0])
+                        pred_class_idx = np.argmax(np.array(relations_probs))
+                        p = relations_probs[pred_class_idx]
+                        if p >= self.thr:
+                            relations_doc.append(
+                                RelationSpan(start=e1, end=e2, score=p, relation=self.relations[pred_class_idx])
+                            )
             relations_pred.append(relations_doc)
         return relations_pred
