21ff301436126e066d2af87f2852acb63e117c04
Gabriele Picco <piccogabriele@gmail.com>
2022-12-27 19:30:33 +0100

    :bug: Reduce test resources

--- zshot/tests/linker/test_regen_linker.py
+++ zshot/tests/linker/test_regen_linker.py
@@ -8,6 +8,7 @@ import spacy
 
 from zshot import PipelineConfig
 from zshot.linker.linker_regen.linker_regen import LinkerRegen
+from zshot.linker.linker_regen.trie import Trie
 from zshot.linker.linker_regen.utils import load_wikipedia_trie, spans_to_wikipedia
 from zshot.mentions_extractor import MentionsExtractorSpacy
 from zshot.tests.config import EX_DOCS, EX_ENTITIES
@@ -46,53 +47,6 @@ def test_regen_linker():
     del doc, nlp, config
 
 
-def test_regen_linker_wikification():
-    nlp = spacy.blank("en")
-    trie = load_wikipedia_trie()
-    config = PipelineConfig(
-        mentions_extractor=DummyMentionsExtractor(),
-        linker=LinkerRegen(trie=trie),
-    )
-    nlp.add_pipe("zshot", config=config, last=True)
-    assert "zshot" in nlp.pipe_names
-
-    doc = nlp(EX_DOCS[1])
-    assert len(doc.ents) > 0
-    del nlp.get_pipe('zshot').mentions_extractor, nlp.get_pipe('zshot').entities, nlp.get_pipe('zshot').nlp
-    del nlp.get_pipe('zshot').linker.tokenizer, nlp.get_pipe('zshot').linker.trie, \
-        nlp.get_pipe('zshot').linker.model, nlp.get_pipe('zshot').linker
-    nlp.remove_pipe('zshot')
-    del doc, nlp, config
-
-
-def test_regen_linker_wikification_with_links():
-    nlp = spacy.blank("en")
-    trie = load_wikipedia_trie()
-    config = PipelineConfig(
-        mentions_extractor=DummyMentionsExtractor(),
-        linker=LinkerRegen(trie=trie),
-    )
-    nlp.add_pipe("zshot", config=config, last=True)
-    assert "zshot" in nlp.pipe_names
-
-    doc = nlp(EX_DOCS[1])
-    assert len(doc.ents) > 0
-    assert len(doc._.spans) > 0
-    assert len(spans_to_wikipedia(doc._.spans)) > 0
-    del nlp.get_pipe('zshot').mentions_extractor, nlp.get_pipe('zshot').entities, nlp.get_pipe('zshot').nlp
-    del nlp.get_pipe('zshot').linker.tokenizer, nlp.get_pipe('zshot').linker.trie, \
-        nlp.get_pipe('zshot').linker.model, nlp.get_pipe('zshot').linker
-    nlp.remove_pipe('zshot')
-    del doc, nlp, config
-
-
-def test_span_to_wiki():
-    s = Span(label="Surfing", start=0, end=10)
-    wiki_links = spans_to_wikipedia([s])
-    assert len(wiki_links) > 0
-    assert wiki_links[0].startswith("https://en.wikipedia.org/wiki?curid=")
-
-
 def test_regen_linker_pipeline():
     nlp = spacy.load("en_core_web_sm")
     config = PipelineConfig(
@@ -110,3 +64,36 @@ def test_regen_linker_pipeline():
         nlp.get_pipe('zshot').linker.model, nlp.get_pipe('zshot').linker
     nlp.remove_pipe('zshot')
     del docs, nlp, config
+
+
+def test_regen_linker_wikification():
+    nlp = spacy.blank("en")
+    trie = Trie()
+    trie.add([0, 1, 2, 3])
+    trie.add([0, 2, 2, 3])
+    config = PipelineConfig(
+        mentions_extractor=DummyMentionsExtractor(),
+        linker=LinkerRegen(trie=trie),
+    )
+    nlp.add_pipe("zshot", config=config, last=True)
+    assert "zshot" in nlp.pipe_names
+
+    doc = nlp(EX_DOCS[1])
+    assert len(doc.ents) > 0
+    del nlp.get_pipe('zshot').mentions_extractor, nlp.get_pipe('zshot').entities, nlp.get_pipe('zshot').nlp
+    del nlp.get_pipe('zshot').linker.tokenizer, nlp.get_pipe('zshot').linker.trie, \
+        nlp.get_pipe('zshot').linker.model, nlp.get_pipe('zshot').linker
+    nlp.remove_pipe('zshot')
+    del doc, nlp, config
+
+
+def test_load_wikipedia_trie():
+    trie = load_wikipedia_trie()
+    assert len(list(trie.trie_dict.keys())) == 6952
+
+
+def test_span_to_wiki():
+    s = Span(label="Surfing", start=0, end=10)
+    wiki_links = spans_to_wikipedia([s])
+    assert len(wiki_links) > 0
+    assert wiki_links[0].startswith("https://en.wikipedia.org/wiki?curid=")
