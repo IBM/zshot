03027a07a9f04c29547096929cdafd253abadf19
Gabriele Picco <piccogabriele@gmail.com>
2022-12-27 18:24:11 +0100

    :bug: Fix Wikification

--- zshot/tests/linker/test_regen_linker.py
+++ zshot/tests/linker/test_regen_linker.py
@@ -64,11 +64,32 @@ def test_regen_linker_wikification():
     del doc, nlp, config
 
 
+def test_regen_linker_wikification_with_links():
+    nlp = spacy.load("en_core_web_sm")
+    trie = load_wikipedia_trie()
+    config = PipelineConfig(
+        mentions_extractor=MentionsExtractorSpacy(),
+        linker=LinkerRegen(trie=trie),
+    )
+    nlp.add_pipe("zshot", config=config, last=True)
+    assert "zshot" in nlp.pipe_names
+
+    doc = nlp(EX_DOCS[1])
+    assert len(doc.ents) > 0
+    assert len(doc._.spans) > 0
+    assert len(spans_to_wikipedia(doc._.spans)) > 0
+    del nlp.get_pipe('zshot').mentions_extractor, nlp.get_pipe('zshot').entities, nlp.get_pipe('zshot').nlp
+    del nlp.get_pipe('zshot').linker.tokenizer, nlp.get_pipe('zshot').linker.trie, \
+        nlp.get_pipe('zshot').linker.model, nlp.get_pipe('zshot').linker
+    nlp.remove_pipe('zshot')
+    del doc, nlp, config
+
+
 def test_span_to_wiki():
     s = Span(label="Surfing", start=0, end=10)
     wiki_links = spans_to_wikipedia([s])
     assert len(wiki_links) > 0
-    assert "wikipedia.org" in wiki_links[0]
+    assert wiki_links[0].startswith("https://en.wikipedia.org/wiki?curid=")
 
 
 def test_regen_linker_pipeline():
