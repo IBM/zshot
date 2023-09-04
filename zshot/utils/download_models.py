62b566c97746b3782c02776b453d07d40f70812c
Marcos Martinez <Marcos.Martinez.Galindo@ibm.com>
2023-09-04 10:40:18 +0100

    Added try/except on load models
    Signed-off-by: Marcos Martinez <Marcos.Martinez.Galindo@ibm.com>

--- zshot/utils/download_models.py
+++ zshot/utils/download_models.py
@@ -1,20 +1,38 @@
 from zshot.evaluation.dataset import load_few_rel_zs, load_medmentions_zs, load_ontonotes_zs
 from zshot.linker import LinkerRegen, LinkerSMXM, LinkerTARS
-from zshot.relation_extractor.relation_extractor_zsrc import RelationsExtractorZSRC
-from zshot.mentions_extractor.utils import ExtractorType
 from zshot.mentions_extractor import MentionsExtractorFlair
+from zshot.mentions_extractor.utils import ExtractorType
+from zshot.relation_extractor.relation_extractor_zsrc import RelationsExtractorZSRC
 
 
 def load_all():
     load_few_rel_zs()
     load_medmentions_zs()
     load_ontonotes_zs()
-    LinkerSMXM().load_models()
-    LinkerRegen().load_models()
-    LinkerTARS().load_models()
-    RelationsExtractorZSRC().load_models()
-    MentionsExtractorFlair(ExtractorType.NER).load_models()
-    MentionsExtractorFlair(ExtractorType.POS).load_models()
+    try:
+        LinkerSMXM().load_models()
+    except RuntimeError:
+        pass
+    try:
+        LinkerRegen().load_models()
+    except RuntimeError:
+        pass
+    try:
+        LinkerTARS().load_models()
+    except RuntimeError:
+        pass
+    try:
+        RelationsExtractorZSRC().load_models()
+    except RuntimeError:
+        pass
+    try:
+        MentionsExtractorFlair(ExtractorType.NER).load_models()
+    except RuntimeError:
+        pass
+    try:
+        MentionsExtractorFlair(ExtractorType.POS).load_models()
+    except RuntimeError:
+        pass
 
 
 if __name__ == "__main__":
