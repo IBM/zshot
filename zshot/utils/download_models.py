49e4b96ff9b40520a19d54050fa8383e09fa5f9a
Marcos Martínez Galindo <marcosmartinezgalindo@Marcoss-MacBook-Pro.local>
2024-08-15 10:41:21 +0100

    Added GLiNER to download_models
    Signed-off-by: Marcos Martínez Galindo <marcosmartinezgalindo@Marcoss-MacBook-Pro.local>

--- zshot/utils/download_models.py
+++ zshot/utils/download_models.py
@@ -1,5 +1,5 @@
 from zshot.evaluation.dataset import load_few_rel_zs, load_medmentions_zs, load_ontonotes_zs
-from zshot.linker import LinkerRegen, LinkerSMXM, LinkerTARS
+from zshot.linker import LinkerRegen, LinkerSMXM, LinkerTARS, LinkerGLINER
 from zshot.mentions_extractor import MentionsExtractorFlair
 from zshot.mentions_extractor.utils import ExtractorType
 from zshot.relation_extractor.relation_extractor_zsrc import RelationsExtractorZSRC
@@ -28,6 +28,10 @@ def load_all():
         pass
     try:
         LinkerTARS().load_models()
+    except RuntimeError:
+        pass
+    try:
+        LinkerGLINER().load_models()
     except RuntimeError:
         pass
     try:
