f21f1fce78439ec505b19c0f669706aef9e97760
Marcos Martinez <Marcos.Martinez.Gaindo@ibm.com>
2025-05-01 03:24:56 +0100

    :sparkles: Add Pile BIO NER Dataset
    Signed-off-by: Marcos Martinez <Marcos.Martinez.Gaindo@ibm.com>

--- zshot/evaluation/__init__.py
+++ zshot/evaluation/__init__.py
@@ -1,3 +1,4 @@
 from zshot.evaluation.dataset import load_medmentions_zs  # noqa: F401
 from zshot.evaluation.dataset import load_ontonotes_zs  # noqa: F401
 from zshot.evaluation.dataset import load_few_rel_zs  # noqa: F401
+from zshot.evaluation.dataset import load_pile_ner_biomed_zs  # noqa: F401
